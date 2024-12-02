a2abfef1e5e397a73641d9894ab695beb5282951
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-12-02 13:41:16 -0600

    Found Carmen Sandiego

--- clues.sql
+++ clues.sql
@@ -1,26 +1,85 @@
--- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+-- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. 
+-- She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, 
+-- and we'll start looking for her there.
  
 -- Write SQL query here
 
+SELECT name, population 
+FROM countries 
+WHERE region = 'Southern Europe' 
+ORDER BY population ASC
+LIMIT 1; 
+/* Should give me the ONE country with the lowest population */
 
--- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+-- Lowest population
+/* Holy See (Vatican City State) */
+
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's
+-- officially recognized language. Check our databases and find out what language is spoken in this country, 
+-- so we can call in a translator to work with you.
+
+-- Write SQL query here
+
+SELECT countries.name AS country, countrylanguages.language AS language
+FROM countries
+JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE countries.name = 'Holy See (Vatican City State)' and countrylanguages.isofficial;
+
+/* Italian */
+
+
+-- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, 
+-- a country where people speak only the language she was learning. Find out which nearby country 
+-- speaks nothing but that language.
 
 -- Write SQL query here
 
+/* Find all southern european countries that speak italian */
+SELECT countries.name AS country, countrylanguages.language AS language
+FROM countries
+JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE countrylanguages.language = 'Italian' AND countries.region = 'Southern Europe';
 
--- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+/* I got Italy, Holy See, and 'San Marino'
+Did another query (Below) to confirm the only other country that exclusively speaks Italian is 'San Marino' */
+
+SELECT countries.name AS country, countrylanguages.language AS language
+FROM countries
+JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE countries.region = 'Southern Europe';
+
+
+
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. 
+-- There are only two cities she could be flying to in the country. One is named the same as the country – 
+-- that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 
+SELECT countries.name AS country, cities.name AS city
+FROM countries
+JOIN cities ON countries.code = cities.countrycode
+WHERE countries.name = 'San Marino' and cities.name != 'San Marino';
+-- ANSWER - 'Serravalle'
 
--- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+
+-- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, 
+-- but in totally different parts of the globe! She's headed to South America as we speak; 
+-- go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, 
+-- and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
 
+SELECT countries.name AS country, cities.name AS city
+FROM countries
+JOIN cities ON countries.code = cities.countrycode
+WHERE countries.region = 'South America' and cities.name LIKE 'Se%';
 
--- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+/* Country - Brazil
 
--- Write SQL query here
+Cities - Serra, Sete Lagoas, Sertãozinho
+
+... I have a feeling it's 'Serra' */
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -29,8 +88,26 @@
 
 -- Write SQL query here
 
+SELECT name, capital 
+FROM countries 
+WHERE name = 'Brazil';
 
--- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
+WHERE countries.capital = cities.id
+
+/* Returns 211, the capital's code apparently */
+
+SELECT countries.name AS country, cities.name AS capital
+FROM countries
+JOIN cities ON countries.code = cities.countrycode
+WHERE countries.name = 'Brazil' AND countries.capital = cities.id;
+
+-- ANSWER = Brasília
+
+
+-- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, 
+-- and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading 
+-- and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure 
+-- she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
 
 --               Our playdate of late has been unusually fun –
@@ -41,4 +118,20 @@
 --               In a city of ninety-one thousand and now, eighty five.
 
 
--- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her 
+-- at the gates with bells on.
+
+/* The riddle says it's a city with a population of 91085
+'And now' means that includes her. So answer is a city with pop of 91084 or 91086 */
+
+SELECT name, population 
+FROM cities
+
+SELECT countries.name AS country, cities.district as district, cities.name AS city
+FROM countries
+JOIN cities ON countries.code = cities.countrycode
+WHERE cities.population = 91084; 
+
+/* === ANSWER ===
+
+California, USA, Santa Monica */
